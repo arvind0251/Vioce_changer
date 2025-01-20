bc0afb54e1832090d433e4f41138e1951be5e7ab
arvind0251 <160855021+arvind0251@users.noreply.github.com>
2025-01-20 08:19:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import os
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackContext
+from telegram.ext.filters import Filters
 from pydub import AudioSegment
 from dotenv import load_dotenv
 
